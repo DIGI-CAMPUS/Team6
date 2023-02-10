fa601177a31bd5db0e9a2a23e7e0c82515f44ce7
moonrise0 <moonriseisnow@gmail.com>
2023-02-10 17:45:54 +0900

    생성

---
+++ daeho/0210/jsin/calculate.js
@@ -0,0 +1,20 @@
+"use strict;";
+
+let a = parseInt(prompt("첫번째"));
+let b = parseInt(prompt("두번째"));
+
+console.log(a);
+console.log(b);
+
+function plus() {
+  alert(a + b);
+}
+function minus() {
+  alert(a - b);
+}
+function multiply() {
+  alert(a * b);
+}
+function division() {
+  alert(a / b);
+}
